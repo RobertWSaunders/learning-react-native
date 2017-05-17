46d0833163caa84935cd66ca69db426851b9c2a2
Robert Saunders <robertwsaunders@outlook.com>
2017-05-16 20:03:40 -0400

    learning reducers and added a boiler plate redux app

---
+++ tech_stack/src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import {creatStore } from 'redux';
+import reducers from './reducers';
+
+
+
+const App = () => {
+  return (
+    <Provider store={creatStore(reducers)}>
+      <View />
+    </Provider>
+  )
+};
+
+export default App;
