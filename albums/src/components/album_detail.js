f1a9bc15d14f10f5d9d1a04b76ed0cbd5720ef5c
Robert Saunders <robertwsaunders@outlook.com>
2017-05-16 22:25:21 -0400

    Added the card and card section components for albums app

--- albums/src/components/album_detail.js
+++ albums/src/components/album_detail.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './card';
+import CardSection from './card_section';
 
 const AlbumDetail = (props) => {
   return (
-    <Text>{props.album.title}</Text>
+    <Card>
+      <CardSection>
+        <Text>{props.album.title}</Text>
+      </CardSection>
+    </Card>
   );
 };
 
