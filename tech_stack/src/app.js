83e5cfb6085f7fbdf7216be95d11829b0ae365ab
Robert Saunders <robertwsaunders@outlook.com>
2017-05-19 10:39:01 -0400

    added common components and redux connect

--- tech_stack/src/app.js
+++ tech_stack/src/app.js
@@ -3,12 +3,17 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import {creatStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 
 const App = () => {
   return (
     <Provider store={creatStore(reducers)}>
-      <View />
+      <View>
+        <Header headerText="Tech Stack"/>
+        <LibraryList />
+      </View>
     </Provider>
   )
 };
